892d3347c1fbc8e4c4832cb4910dc721f0db57b3
Alex Fructo <alexfructo@gmail.com>
2023-03-13 14:35:21 -0300

    Adicionada a função para receber e tratar os argumentos de entrada do script

--- source/modules/utils/utils.py
+++ source/modules/utils/utils.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime, timedelta
 
 DATE_FORMAT = '%d-%m-%Y'
@@ -23,3 +24,29 @@ def validate_date_period(start_date: str, end_date: str, max_period: int) -> boo
 def validate_action(action: str) -> bool:
     allowed_actions = ['exportar', 'inserir']
     return action in allowed_actions
+
+def parse_args(max_period=7):
+    parser = argparse.ArgumentParser(description='Data Exporter')
+    parser.add_argument('-start_date', '--start_date', type=str, required=True, help='Data inicial (DD-MM-YYYY)', default='09-03-2023')
+    parser.add_argument('-end_date', '--end_date', type=str, required=True, help='Data final (DD-MM-YYYY)', default='09-03-2023')
+    parser.add_argument('-report', '--report', type=str, required=True, choices=['central_historico', 'central_atividades', 'portal_atendimentos', 'vonage_sessoes'], help='Tipo de relatório', default='central_historico')
+    parser.add_argument('-action', '--action', type=str, required=True, choices=['exportar', 'inserir'], help='Tipo de ação', default='exportar')
+    
+    try:
+        args = parser.parse_args()
+
+        if not validate_date_format(args.start_date):
+            raise ValueError('Data inicial deve estar no formato DD-MM-YYYY')
+
+        if not validate_date_format(args.end_date):
+            raise ValueError('Data final deve estar no formato DD-MM-YYYY')
+
+        if not validate_date_range(args.start_date, args.end_date):
+            raise ValueError('Data inicial não pode ser maior que a final')
+
+        if not validate_date_period(args.start_date, args.end_date, max_period):
+            raise ValueError(f'Período máximo de pesquisa é de {max_period} dias')
+    except ValueError as e:
+        parser.print_help()
+
+    return args
